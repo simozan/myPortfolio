9296ae0f81d3c29db7f7420d015d157a88ab387d
Simone <simone.zanni.87@gmail.com>
2024-07-26 10:10:46 +0200

    update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import styles from './App.module.css'
+import styles from "./App.module.css";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
-
   return (
     <div className={styles.App}>
-    Hello World!
+      <Navbar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
