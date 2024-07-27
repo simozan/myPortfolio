2e86fe2313b8481474b1dd60949cc596a7914831
Simone <simone.zanni.87@gmail.com>
2024-07-27 12:57:52 +0200

    assets update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,41 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import styles from "./Navbar.module.css"
+import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utilis";
 
 export const Navbar = () => {
-  cont [menuOpen, setMenuOpen] = useState(false)
+  const[menuOpen, setMenuOpen] = useState(false);
   return (
-  <nav className={styles.navbar}>
-    <a className={styles.title} href="/">Portfolio</a>
-    <div className={styles.menu}>
-      <img 
-      className={styles.menuBtn} 
-      src={manuOpen ? getImageUrl("nav/menuIcon.png")}  alt="menu-button"/>
-      <ul className={styles.menuItems}>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#experience">Experience</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </nav>)
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={()=> setMenuOpen(!menuOPen)}
+        />
+        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
