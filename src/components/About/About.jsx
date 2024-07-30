32d5c0744e44d597146a4a7ddfd34c5fdf3e4a7d
Simone <simone.zanni.87@gmail.com>
2024-07-30 13:47:54 +0200

    end of about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,14 +5,14 @@ import styles from "./About.module.css"
 
 export const About = () => {
   return (
-    <section className={styles.container}>
+    <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
         {" "}
         <img
           src={getImageUrl("about/aboutImage.png")}
           alt="Me sitting with a laptop"
-          className="styles.aboutImage"
+          className={styles.aboutImage}
         />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
