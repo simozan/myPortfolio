df3533544149bfd25521a6e2d170ee28f6075f76
Simone <simone.zanni.87@gmail.com>
2024-07-31 11:12:36 +0200

    update experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import styles from "./Experience.module.css"
 import skills from "../../data/skills.json";
+import history from "../../data/history.json";
+
 import { getImageUrl } from "../../utilis";
 
 export const Experience = () => {
   return (
-    <section id="experience">
-      <h2>Experience</h2>
-      <div>
-        <div>
+    <section className={styles.container} id="experience">
+      <h2 className={styles.title}>Experience</h2>
+      <div className={styles.content}>
+        <div className={styles.skills}>
           {skills.map((skill, id) => {
             return (
               <div key={id}>
@@ -21,7 +24,23 @@ export const Experience = () => {
           })}
         </div>
         <ul>
-            
+          {history.map((historyItem, id) => {
+            return (
+              <li key={id}>
+                <img
+                  src={getImageUrl(historyItem.imagesrc)}
+                  alt={`${historyItem.organization} Logo`}
+                />
+                <div>
+                  <h3>{`${historyItem.role}, ${historyItem.organization}`}</h3>
+                  <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                  <ul>{historyItem.experiences.map((experience, id)=>{
+                    return <li key={id}>{experience}</li>
+                  })}</ul>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </section>
