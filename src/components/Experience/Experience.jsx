a6557cd45e3afb1416c1d172b0ebc691e14b3fd8
Simone <simone.zanni.87@gmail.com>
2024-08-01 08:03:21 +0200

    upodate experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Experience.module.css"
+import styles from "./Experience.module.css";
 import skills from "../../data/skills.json";
 import history from "../../data/history.json";
 
@@ -28,15 +28,17 @@ export const Experience = () => {
             return (
               <li key={id} className={styles.historyItem}>
                 <img
-                  src={getImageUrl(historyItem.imagesrc)}
-                  alt={`${historyItem.organization} Logo`}
+                  src={getImageUrl(historyItem.imageSrc)}
+                  alt={`${historyItem.organisation} Logo`}
                 />
                 <div className={styles.historyItemDetails}>
-                  <h3>{`${historyItem.role}, ${historyItem.organization}`}</h3>
+                  <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
                   <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
-                  <ul>{historyItem.experiences.map((experience, id)=>{
-                    return <li key={id}>{experience}</li>
-                  })}</ul>
+                  <ul>
+                    {historyItem.experiences.map((experience, id) => {
+                      return <li key={id}>{experience}</li>;
+                    })}
+                  </ul>
                 </div>
               </li>
             );
