a83008cb479c44b9482bcddfb2c1e82ead9e83ee
Simone <simone.zanni.87@gmail.com>
2024-08-11 10:03:37 +0200

    update end projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,7 +7,7 @@ import { ProjectCard } from "./ProjectCard";
 
 export const Projects = (props) => {
   return (
-    <section className={styles.container}>
+    <section className={styles.container} id="projects">
       <h2 className={styles.title}>Projects</h2>
       <div className={styles.projects}>
         {projects.map((project, id) => {
