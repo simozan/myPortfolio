71a06572fb06f94ad1c0497831219c8a35cc37a5
Simone <simone.zanni.87@gmail.com>
2024-08-13 19:34:26 +0200

    update skills

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { About } from './components/About/About';
 import { Experience } from './components/Experience/Experience';
 import { Projects } from './components/Projects/Projects';
 import { Contact } from './components/Contact/Contact';
+import { Skills } from './components/Skills/Skills';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
     <Navbar/>
     <Hero />
     <About />
+    <Skills />
     <Experience />
     <Projects />
     <Contact />
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -10,19 +10,6 @@ export const Experience = () => {
     <section className={styles.container} id="experience">
       <h2 className={styles.title}>Experience</h2>
       <div className={styles.content}>
-        <div className={styles.skills}>
-          {skills.map((skill, id) => {
-            return (
-              <div key={id} className={styles.skill}>
-                <div className={styles.skillImageContainer}>
-                  {" "}
-                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
-                </div>
-                <p>{skill.title}</p>
-              </div>
-            );
-          })}
-        </div>
         <ul className={styles.history}>
           {history.map((historyItem, id) => {
             return (
