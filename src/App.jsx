3c91d553f33289fcddfe8718bec33171cc4ac02f
Simone <simone.zanni.87@gmail.com>
2024-10-06 13:29:35 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import styles from './App.module.css';
 import { Navbar } from './components/Navbar/Navbar';
 import { Hero } from './components/Hero/Hero';
-import { About } from './components/About/About';
-import { Experience } from './components/Experience/Experience';
-import { Projects } from './components/Projects/Projects';
+import { About } from './pages/About/About';
+import { Experience } from './pages/Experience/Experience';
+import { Projects } from './pages/Projects/Projects';
 import { Contact } from './components/Contact/Contact';
 import { Skills } from './components/Skills/Skills';
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import App from './App.jsx'
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css'
+
 
 import "@fontsource/outfit"
 import "@fontsource/roboto"
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Router>
     <App />
-  </React.StrictMode>,
-)
+  </Router>
+);
